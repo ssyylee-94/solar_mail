77e230ae3a0deee48d55e561c54d892c5c832cdc
sunyeong <63041465+ssyylee-94@users.noreply.github.com>
2020-09-17 11:43:49 +0900

    upload

---
+++ settings.py
@@ -0,0 +1,18 @@
+# ----------- mysql ------------
+user_id = "id"  # id
+pw = "pw"  # pass word
+url = "aws-rds"  # end point
+
+name1 = "solarmail_test"  # DB name
+name2 = "kma_db"  # DB name
+
+region_dict = {'seoul': '서울',
+               'busan': '부산',
+               'daejun': '대전',
+               'daegu': '대구',
+               'gwangju': '광주',
+               'ulsan': '울산',
+               'incheon': '인천',
+               'cheongju': '청주',
+               'chuncheon': '춘천',
+               'jeonju': '전주'}
